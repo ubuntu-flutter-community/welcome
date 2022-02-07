6921260232cd4af01b5ab87c7c63fd629bbbba2e
Joshua Peisach <itzswirlz2020@outlook.com>
2022-02-07 17:20:17 -0500

    Add bottom row + ubuntu image

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,38 +11,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: const Center(
+        child: Image(
+          image: AssetImage('assets/ubuntu.png'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomNavigationBar: const BottomAppBar(
+        color: YaruColors.blue,
+        child: Text('Social Media Link Row Here....'),
       ),
     );
   }
